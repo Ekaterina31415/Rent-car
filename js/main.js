817324cde665e09add57ede80ee3be8d030fc11c
Kate K <kilina2018@bk.ru>
2020-06-05 01:42:42 +0400

    refact: fixed first slider and hero video

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper('.swiper-container', {
-    loop: true
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
   })
+
+  var swiper = new Swiper('.swiper-about', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 });
