da36149f961a78d80cce40657359f5b30691265f
Kate K <kilina2018@bk.ru>
2020-06-15 13:15:35 +0400

    feat: footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-  //initialize swiper when document ready
   var mySwiper = new Swiper('.swiper-container', {
     loop: true,
     navigation: {
@@ -18,4 +17,27 @@ $(document).ready(function () {
       prevEl: '.button-prev',
     },
   });
+
+  var selectBody = $('.select__body'),
+      selectBtn = $('[data-toggle="select-list"]');
+
+    selectBtn.on('click', function () {
+      selectBody.toggleClass('select__body--visible');
+    });
+
+  let selectItem = $('.select__item');
+
+  selectItem.on('click', function () {
+    selectBody.toggleClass('select__body--visible');
+  });
+
+  function selectChoose() {
+    let text = this.innerText,
+    select = = this.closest('.select'),
+    currentText = this.closest('.select').querySelector(selectors: '
+    .select__current');
+    currentText.innerText = text;
+    select.classList.remove(token: 'is-active');
+  }
+
 });
