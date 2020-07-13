efb8455b9cd261a24d59b609dc842961469970e5
Kate K <kilina2018@bk.ru>
2020-07-13 12:25:06 +0400

    feat: finished layout

--- js/main.js
+++ js/main.js
@@ -18,6 +18,17 @@ $(document).ready(function () {
     },
   });
 
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
   var selectBody = $('.select__body'),
       selectBtn = $('[data-toggle="select-list"]');
 
@@ -27,17 +38,52 @@ $(document).ready(function () {
 
   let selectItem = $('.select__item');
 
-  selectItem.on('click', function () {
-    selectBody.toggleClass('select__body--visible');
+  selectItem.on('click', (e) => {
+    let text = e.currentTarget.innerHTML;
+    select = e.currentTarget.closest('.select'),
+      currentText = e.currentTarget.closest('.select').querySelector('.select__current');
+      currentText.innerHTML = text;
+      select.classList.remove('select__body--visible');
   });
+  
+  $('.1day').on('click', function () {
+    $('.currently-sum-num').text('300');
+    $('.prev-sum').text('400');
+    $('#select-input').value = '1 день';
+  })
+  $('.3days').on('click', function () {
+    $('.currently-sum-num').text('500');
+    $('.prev-sum').text('600');
+    $('#select-input').value = '3 дня';
+  })
+  $('.5days').on('click', function () {
+    $('.currently-sum-num').text('900');
+    $('.prev-sum').text('1000');
+    $('#select-input').value = '5 дней';
+  })
+  $('.7days').on('click', function () {
+    $('.currently-sum-num').text('1400');
+    $('.prev-sum').text('1600');
+    $('#select-input').value = '7 дней';
+  })
+  $('.10days').on('click', function () {
+    $('.currently-sum-num').text('1900');
+    $('.prev-sum').text('2280');
+    $('#select-input').value = '10 дней';
+  })
 
-  function selectChoose() {
-    let text = this.innerText,
-    select = = this.closest('.select'),
-    currentText = this.closest('.select').querySelector(selectors: '
-    .select__current');
-    currentText.innerText = text;
-    select.classList.remove(token: 'is-active');
-  }
+//  Ховер для первого видео
+  $('.video__play').on('mouseenter', function () {
+    $(this).animate({'width': '258px'});
+  })
+  $('.video__play').on('mouseleave', function () {
+    $(this).animate({ 'width': '248px' });
+  })
+// Промокод
+$('.promocode').on('click', function () {
+  $(this).css({'display': 'none'});
+  $('#promocode-input').fadeIn(1000);
+})
 
+// в отписках валидация и маска
 });
